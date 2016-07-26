646f7873db36543295c759bd436cb81dd00b6ea6
Gabriel Colloca <Gabriel Colloca>
2016-07-26 01:36:33 -0300

    cors

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ var DEVICES_COLLECTION = "devices";
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 }
 
